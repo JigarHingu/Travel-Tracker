8faf919555b334354a8df920d1c2c93b430ce895
Jigar Hingu <jigarhingu46.com>
2024-04-16 11:26:21 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const pg = require('pg');
+
+const app = express();
+const port = process.env.PORT || 3000; // Assuming you have a PORT variable defined in your environment or default to 3000
+
+// Database connection setup
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+db.connect();
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+async function checkVisited() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  const countries = result.rows.map(country => country.country_code);
+  return countries;
+}
+
+async function getTotalCountries() {
+  const result = await db.query("SELECT COUNT(*) AS total FROM visited_countries");
+  return result.rows[0].total;
+}
+
+// Routes
+app.get("/", async (req, res) => {
+  const countries = await checkVisited();
+  res.render("index.ejs", { countries: countries, total: countries.length });
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) = LOWER($1)",
+      [input]
+    );
+
+    if (result.rows.length === 0) {
+      return res.render("index.ejs", { error: "Country not found.", countries: await checkVisited(), total: await getTotalCountries() });
+    }
+
+    const countryCode = result.rows[0].country_code;
+
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      );
+      res.render("index.ejs", { success: "Country added successfully.", countries: await checkVisited(), total: await getTotalCountries() });
+    } catch (err) {
+      console.log(err);
+      res.render("index.ejs", { error: "Already added.", countries: await checkVisited(), total: await getTotalCountries() });
+    }
+  } catch (err) {
+    console.log(err);
+    res.render("index.ejs", { error: "Database error.", countries: await checkVisited(), total: await getTotalCountries() });
+  }
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
